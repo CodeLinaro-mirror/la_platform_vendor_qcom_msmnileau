be786e6277224dfc49f8ce42e50bc20288972f58
Puneet Yatnal <puneet@codeaurora.org>
2020-04-28 20:46:06 +0530

    enabled the sensor config for msmnile_au auto target
    Change-Id: I89d5e57bce7a6bf034582f4dfdddb740f0291146
    Signed-off-by: Puneet Yatnal <puneet@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -223,6 +223,9 @@ TARGET_USES_GRALLOC1 := true
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := false
 
+# Enable sensor Version V_1
+USE_SENSOR_HAL_VER := 1.0
+
 #Add non-hlos files to ota packages
 ADD_RADIO_FILES := true
 
--- msmnile_au.mk
+++ msmnile_au.mk
@@ -232,6 +232,12 @@ PRODUCT_PACKAGES += \
        gptp \
        mrpd
 
+# Sensor conf files
+PRODUCT_COPY_FILES += \
+    device/qcom/msmnile_au/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml
+
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
 KERNEL_MODULES_OUT := out/target/product/msmnile_au/$(KERNEL_MODULES_INSTALL)/lib/modules
