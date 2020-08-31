71017e35845503ca8c45df78e332b6eb918f05e9
Aravind Vijayakumar <aprasann@codeaurora.org>
2020-08-30 23:13:28 -0700

    msmnile_au:Cmd line change for bootup memory for mem hotplug
    Adding early paramters to kernel command line indicating the
    no hotplug area using a flag, the flag is disabled by default.
    As per the parameters, the initial bootup memory is 2GB,
    remaining 6 GB is hotplugged later once the bootup is
    completed.
    
    Signed-off-by: Aravind Vijayakumar <aprasann@codeaurora.org>
    Change-Id: I5a7a563bd3a705d96ad73f249e1b3b98996bcbcb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,6 +189,10 @@ TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := false
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.recover_usb=1 androidboot.selinux=enforcing
+MEMORY_HOTPLUG_2G := false
+ifeq ($(MEMORY_HOTPLUG_2G),true)
+BOARD_KERNEL_CMDLINE += no_hotplug_area=0x80000000,0x40000000 dyn_memhotplug
+endif
 
 BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
 
