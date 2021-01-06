f389f9c65b5847c647130eb7e6ad48434435fc30
Tabassum Tabassum <ttabassu@codeaurora.org>
2021-01-06 11:52:50 +0530

    msmnile_au : Enabling API level to  31
    Updated shipping api level 31
    
    Change-Id: I0ea8519d152ed453f063beeae4470496dbdb5dc6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -251,7 +251,7 @@ endif
 
 #Flag to enable System SDK Requirements.
 #All vendor APK will be compiled against system_current API set.
-BOARD_SYSTEMSDK_VERSIONS:=29
+BOARD_SYSTEMSDK_VERSIONS:= S
 
 #Enable VNDK Compliance
 BOARD_VNDK_VERSION:=current
