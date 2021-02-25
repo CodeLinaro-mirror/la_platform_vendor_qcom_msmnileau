f98ed58f064bfcbdab2fa200adbdefe374a9375e
Suraj Jaiswal <jsuraj@codeaurora.org>
2021-02-25 19:22:00 +0530

    msmnileau: init script change
    Change init.target.rc for rps setting.
    
    Change-Id: Ie3e62bc7f8b8c130552c07311d335bab3c266997

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -300,6 +300,9 @@ endif
 PRODUCT_PACKAGES += \
     emac_perf_settings.sh
 
+#for Emac
+PRODUCT_PACKAGES += emac_rps_settings.sh
+
 # CAN utils
 PRODUCT_PACKAGES += candump \
                     cansend \
