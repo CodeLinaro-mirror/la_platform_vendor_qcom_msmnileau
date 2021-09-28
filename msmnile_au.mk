940e6ac974bc1c8f2d4cd487eb498ec6af17874b
Asmeer Mohammed <asmemoha@codeaurora.org>
2021-09-28 11:07:47 +0530

    msmnile_au: vts_treble_vintf_vendor_test fix
    - Removed wifilearner hal entry
    - vendor.qti.diaghal disabled for user build
    - wpa_supplicant rc file from external/wpa_supplicant_8/ enabled
    
    Change-Id: I3558bc6dffe94666b28c689786d47bd47169a321

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -112,7 +112,9 @@ KERNEL_LLVM_SUPPORT := true
 KERNEL_SD_LLVM_SUPPORT := false
 
 # diag-router
-TARGET_HAS_DIAG_ROUTER := true
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    TARGET_HAS_DIAG_ROUTER := true
+endif
 
 # Target uses DIAG_MDM2 instance to collect WLAN fw diag logs
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.diag_mdm.inst.name=diag_mdm2
@@ -312,6 +314,9 @@ ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 TARGET_WLAN_CHIP := qca6174 qca6390 qcn7605
 include device/qcom/wlan/msmnile_au/wlan.mk
 endif
+
+#Enable rc file from wpa_supplicant project
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 #for Emac
 PRODUCT_PACKAGES += \
