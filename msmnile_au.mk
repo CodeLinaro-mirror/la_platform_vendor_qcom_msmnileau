0beba8c253449737d14e9465b66331eb8c4a166c
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2022-10-14 12:21:07 +0530

    msmnile_au : Enabling GAS properties
    Change-Id: Ib51ed0dfbfe0e02e9a5a8c6b9cedfe92046658f7

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -547,6 +547,11 @@ PRODUCT_VENDOR_PROPERTIES += ro.radio.noril=true
 # Default wifi country code
 PRODUCT_VENDOR_PROPERTIES += ro.boot.wificountrycode=us
 
+#Copy supported features list
+ifeq ($(TARGET_USES_GAS),true)
+PRODUCT_COPY_FILES += device/qcom/msmnile_au/msmnile_au_features.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/msmnile_au_features.xml
+endif
+
 #for Emac
 PRODUCT_PACKAGES += emac_perf_settings.sh
 
