582bdaeab4bbbd757dbada709333138d283b5030
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2022-11-02 16:23:55 +0530

    msmnile_au : Replace product_property_override
    We need to use "PRODUCT_VENDOR_PROPERTIES"
    instead of PRODUCT_PROPERTY_OVERRIDES.
    This will deprecated and it will not allow
    vendor properties to be included in build.
    
    Change-Id: I22f3a64a2d803696db194de1cf8347377bd319ab

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -102,7 +102,7 @@ endif
 TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, device/qcom/common/common64.mk)
 #Inherit all except heap growth limit from phone-xhdpi-2048-dalvik-heap.mk
-PRODUCT_PROPERTY_OVERRIDES  += \
+PRODUCT_VENDOR_PROPERTIES  += \
 	dalvik.vm.heapstartsize=8m \
 	dalvik.vm.heapsize=512m \
 	dalvik.vm.heaptargetutilization=0.75 \
@@ -141,7 +141,7 @@ KERNEL_SD_LLVM_SUPPORT := false
 TARGET_HAS_DIAG_ROUTER := true
 
 # Target uses DIAG_MDM2 instance to collect WLAN fw diag logs
-PRODUCT_PROPERTY_OVERRIDES += vendor.usb.diag_mdm.inst.name=diag_mdm2
+PRODUCT_VENDOR_PROPERTIES += vendor.usb.diag_mdm.inst.name=diag_mdm2
 
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
@@ -347,7 +347,7 @@ TARGET_WLAN_CHIP := qca6390
 include device/qcom/wlan/msmnile_au/wlan.mk
 endif
 
-PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+PRODUCT_VENDOR_PROPERTIES += rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
                             persist.rild.nitz_plmn=
                             persist.rild.nitz_long_ons_0=
                             persist.rild.nitz_long_ons_1=
@@ -362,13 +362,13 @@ PRODUCT_PROPERTY_OVERRIDES += rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
                             dalvik.vm.heapsize=36m \
                             dev.pm.dyn_samplingrate=1
 
-PRODUCT_PROPERTY_OVERRIDES += qcom.hw.aac.encoder=true
+PRODUCT_VENDOR_PROPERTIES += qcom.hw.aac.encoder=true
 
 # Cne module properties
-PRODUCT_PROPERTY_OVERRIDES += persist.vendor.cne.feature=1
+PRODUCT_VENDOR_PROPERTIES += persist.vendor.cne.feature=1
 
 # system properties for MM modules
-PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-player=true \
+PRODUCT_VENDOR_PROPERTIES += media.stagefright.enable-player=true \
                               media.stagefright.enable-http=true \
                               media.stagefright.enable-aac=true \
                               media.stagefright.enable-qcp=true \
@@ -381,50 +381,50 @@ PRODUCT_PROPERTY_OVERRIDES += media.stagefright.enable-player=true \
                               persist.mm.enable.prefetch=true
 
 # system props for the data modules
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.use_data_netmgrd=true \
+PRODUCT_VENDOR_PROPERTIES += ro.vendor.use_data_netmgrd=true \
                               persist.vendor.data.mode=concurrent
 
 # system props for time-services
-PRODUCT_PROPERTY_OVERRIDES += persist.timed.enable=true
+PRODUCT_VENDOR_PROPERTIES += persist.timed.enable=true
 
 # system prop for opengles version
 #
 # 196608 is decimal for 0x30000 to report version 3
 # 196609 is decimal for 0x30001 to report version 3.1
 #  196610 is decimal for 0x30002 to report version 3.2
-PRODUCT_PROPERTY_OVERRIDES += ro.opengles.version=196610
+PRODUCT_VENDOR_PROPERTIES += ro.opengles.version=196610
 
 # system property for maximum number of HFP client connections
-PRODUCT_PROPERTY_OVERRIDES += bt.max.hfpclient.connections=1
+PRODUCT_VENDOR_PROPERTIES += bt.max.hfpclient.connections=1
 
 # Simulate sdcard on /data/media
-PRODUCT_PROPERTY_OVERRIDES += persist.fuse_sdcard=true
+PRODUCT_VENDOR_PROPERTIES += persist.fuse_sdcard=true
 
 # System prop for Bluetooth SOC type
-PRODUCT_PROPERTY_OVERRIDES += vendor.qcom.bluetooth.soc=rome
+PRODUCT_VENDOR_PROPERTIES += vendor.qcom.bluetooth.soc=rome
 
 # System prop for wipower support
-PRODUCT_PROPERTY_OVERRIDES += ro.bluetooth.emb_wp_mode=false \
+PRODUCT_VENDOR_PROPERTIES += ro.bluetooth.emb_wp_mode=false \
                               ro.bluetooth.wipower=false \
                               persist.vendor.service.bt.a2dp.sink=true \
                               persist.vendor.btstack.enable.splita2dp=false \
                               persist.vendor.service.bdroid.sibs=false
 
 # System prop for setting sensor hal
-PRODUCT_PROPERTY_OVERRIDES += ro.hardware.sensors=msmnile.asm_auto \
+PRODUCT_VENDOR_PROPERTIES += ro.hardware.sensors=msmnile.asm_auto \
                               ro.hardware.type=automotive
 
 # Snapdragon value add features
-PRODUCT_PROPERTY_OVERRIDES += ro.qc.sdk.audio.ssr=false
+PRODUCT_VENDOR_PROPERTIES += ro.qc.sdk.audio.ssr=false
 
 # Fluencetype can be "fluence" or "fluencepro" or "none"
-PRODUCT_PROPERTY_OVERRIDES += ro.qc.sdk.audio.fluencetype=none \
+PRODUCT_VENDOR_PROPERTIES += ro.qc.sdk.audio.fluencetype=none \
                               persist.audio.fluence.voicecall=true \
                               persist.audio.fluence.voicerec=false \
                               persist.audio.fluence.speaker=true
 
 # System prop for RmNet Data
-PRODUCT_PROPERTY_OVERRIDES += persist.rmnet.data.enable=true \
+PRODUCT_VENDOR_PROPERTIES += persist.rmnet.data.enable=true \
                               persist.data.wda.enable=true \
                               persist.data.df.dl_mode=5 \
                               persist.data.df.ul_mode=5 \
@@ -435,37 +435,37 @@ PRODUCT_PROPERTY_OVERRIDES += persist.rmnet.data.enable=true \
                               persist.data.df.dev_name=rmnet_usb0
 
 # property to enable user to access Google WFD settings
-PRODUCT_PROPERTY_OVERRIDES += persist.debug.wfd.enable=1
+PRODUCT_VENDOR_PROPERTIES += persist.debug.wfd.enable=1
 
 # property to choose between virtual/external wfd display
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.wfd.virtual=0
+PRODUCT_VENDOR_PROPERTIES += persist.sys.wfd.virtual=0
 
 # Enable tunnel encoding for amrwb
-PRODUCT_PROPERTY_OVERRIDES += tunnel.audio.encode = true
+PRODUCT_VENDOR_PROPERTIES += tunnel.audio.encode = true
 
 # Buffer size in kbytes for compress offload playback
-PRODUCT_PROPERTY_OVERRIDES += audio.offload.buffer.size.kb=32
+PRODUCT_VENDOR_PROPERTIES += audio.offload.buffer.size.kb=32
 
 # Enable offload audio video playback by default
-PRODUCT_PROPERTY_OVERRIDES += av.offload.enable=true
+PRODUCT_VENDOR_PROPERTIES += av.offload.enable=true
 
 # Enable voice path for PCM VoIP by default
-PRODUCT_PROPERTY_OVERRIDES += use.voice.path.for.pcm.voip=true
+PRODUCT_VENDOR_PROPERTIES += use.voice.path.for.pcm.voip=true
 
 # System prop for NFC DT
-PRODUCT_PROPERTY_OVERRIDES += ro.nfc.port=I2C
+PRODUCT_VENDOR_PROPERTIES += ro.nfc.port=I2C
 
 # Enable dsp gapless mode by default
-PRODUCT_PROPERTY_OVERRIDES += audio.offload.gapless.enabled=true
+PRODUCT_VENDOR_PROPERTIES += audio.offload.gapless.enabled=true
 
 # initialize QCA1530 detection
-PRODUCT_PROPERTY_OVERRIDES += sys.qca1530=detect
+PRODUCT_VENDOR_PROPERTIES += sys.qca1530=detect
 
 # Enable stm events
-PRODUCT_PROPERTY_OVERRIDES += persist.debug.coresight.config=stm-events
+PRODUCT_VENDOR_PROPERTIES += persist.debug.coresight.config=stm-events
 
 # Hwui properties
-PRODUCT_PROPERTY_OVERRIDES += ro.hwui.texture_cache_size=72 \
+PRODUCT_VENDOR_PROPERTIES += ro.hwui.texture_cache_size=72 \
                               ro.hwui.layer_cache_size=48 \
                               ro.hwui.r_buffer_cache_size=8 \
                               ro.hwui.path_cache_size=32 \
@@ -479,26 +479,26 @@ PRODUCT_PROPERTY_OVERRIDES += ro.hwui.texture_cache_size=72 \
                               config.disable_rtt=true
 
 # Bringup properties
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.force_sw_gles=1 \
+PRODUCT_VENDOR_PROPERTIES += persist.sys.force_sw_gles=1 \
                               persist.vendor.radio.atfwd.start=true \
                               ro.kernel.qemu.gles=0 \
                               qemu.hw.mainkeys=0
 
 # Increase cached app limit
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.qti.sys.fw.bg_apps_limit=60
+PRODUCT_VENDOR_PROPERTIES += ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # Enable ZRAM
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.qti.config.zram=true
+PRODUCT_VENDOR_PROPERTIES += ro.vendor.qti.config.zram=true
 
 # IOP properties
-PRODUCT_PROPERTY_OVERRIDES += vendor.iop.enable_uxe=1 \
+PRODUCT_VENDOR_PROPERTIES += vendor.iop.enable_uxe=1 \
                               vendor.perf.iop_v3.enable=true
 
 # Property to enable perf boosts from System Server
-PRODUCT_PROPERTY_OVERRIDES += vendor.perf.gestureflingboost.enable=true
+PRODUCT_VENDOR_PROPERTIES += vendor.perf.gestureflingboost.enable=true
 
 # Enable ULMK properties
-PRODUCT_PROPERTY_OVERRIDES += ro.lmk.kill_heaviest_task=true \
+PRODUCT_VENDOR_PROPERTIES += ro.lmk.kill_heaviest_task=true \
                               ro.lmk.kill_timeout_ms=15 \
                               ro.lmk.use_minfree_levels=true \
                               ro.lmk.enhance_batch_kill=true \
@@ -506,36 +506,36 @@ PRODUCT_PROPERTY_OVERRIDES += ro.lmk.kill_heaviest_task=true \
                               ro.lmk.vmpressure_file_min=80640
 
 # Property to enable scroll pre-obtain view
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.scroll.preobtain.enable=true
+PRODUCT_VENDOR_PROPERTIES += ro.vendor.scroll.preobtain.enable=true
 
 # Expose aux camera for below packages
-PRODUCT_PROPERTY_OVERRIDES += vendor.camera.aux.packagelist=org.codeaurora.snapcam
+PRODUCT_VENDOR_PROPERTIES += vendor.camera.aux.packagelist=org.codeaurora.snapcam
 
 # Display mirroring
-PRODUCT_PROPERTY_OVERRIDES += vendor.display.builtin_mirroring=true
+PRODUCT_VENDOR_PROPERTIES += vendor.display.builtin_mirroring=true
 
 # Display hwcId allocation
-PRODUCT_PROPERTY_OVERRIDES += vendor.display.builtin_baseid_and_size=5,3 \
+PRODUCT_VENDOR_PROPERTIES += vendor.display.builtin_baseid_and_size=5,3 \
                               vendor.display.pluggable_baseid_and_size=1,4 \
                               vendor.display.virtual_baseid_and_size=8,1
 
 # Gralloc use dmabuf
-PRODUCT_PROPERTY_OVERRIDES += vendor.gralloc.use_dma_buf_heaps=1
+PRODUCT_VENDOR_PROPERTIES += vendor.gralloc.use_dma_buf_heaps=1
 
 # Disable boot animation
-PRODUCT_PROPERTY_OVERRIDES += debug.sf.nobootanimation=1
+PRODUCT_VENDOR_PROPERTIES += debug.sf.nobootanimation=1
 
 # Enable car power manager for LPM(LowPowerMode)
-PRODUCT_PROPERTY_OVERRIDES += persist.vendor.car.lpm=true
+PRODUCT_VENDOR_PROPERTIES += persist.vendor.car.lpm=true
 
 # Set BT AVRCP prop to false
-PRODUCT_PROPERTY_OVERRIDES += persist.bluetooth.enablenewavrcp=false
+PRODUCT_VENDOR_PROPERTIES += persist.bluetooth.enablenewavrcp=false
 
 # Disable Telephony
-PRODUCT_PROPERTY_OVERRIDES += ro.radio.noril=true
+PRODUCT_VENDOR_PROPERTIES += ro.radio.noril=true
 
 # Default wifi country code
-PRODUCT_PROPERTY_OVERRIDES += ro.boot.wificountrycode=us
+PRODUCT_VENDOR_PROPERTIES += ro.boot.wificountrycode=us
 
 #for Emac
 PRODUCT_PACKAGES += emac_perf_settings.sh
@@ -591,7 +591,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 PRODUCT_PACKAGES_DEBUG += dumpsCaches
 
 # privapp-permissions whitelisting (To Fix CTS :privappPermissionsMustBeEnforced)
-PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
+PRODUCT_VENDOR_PROPERTIES += ro.control_privapp_permissions=enforce
 
 ###################################################################################
 # This is the End of target.mk file.
