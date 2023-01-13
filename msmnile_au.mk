2e310913fb966557ce527c4b7c5ff268ef478f0c
Manjunatha Madana <quic_c_mamanj@quicinc.com>
2023-01-13 11:54:02 +0530

    vintf changes for target level 7 and Android U.
    Change-Id: Ie22100da7a6ef6fe063ff17bad92481fa8f9691a
    (cherry picked from commit 6089fd9456bf1ad9a8ed422f3ce70d8fc0b02a23)

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -579,11 +579,11 @@ PRODUCT_PACKAGES += vndservicemanager
 
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
-PRODUCT_PACKAGES += android.hardware.dumpstate@1.1-service.example \
+PRODUCT_PACKAGES += android.hardware.dumpstate-service.example \
                     android.hardware.thermal@2.0-service.mock \
 
-PRODUCT_PACKAGES += android.hardware.health@2.1-service \
-                    android.hardware.health@2.1-impl \
+PRODUCT_PACKAGES += android.hardware.health-service.qti \
+                    android.hardware.health-service.qti_recovery
 
 PRODUCT_PACKAGES += android.hardware.neuralnetworks@1.0.vendor \
                     android.hardware.neuralnetworks@1.1.vendor \
