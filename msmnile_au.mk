3ca2565aebb85823bfec292ca5806aad0982bf9e
Sravani Mada <quic_smada@quicinc.com>
2024-06-27 11:10:24 +0530

    msmnileau: Update PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD to lz4
    Update PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD from gz to lz4
    for better performace while OTA update
    
    Change-Id: I5a9592c8e10fad4de8bdb69aace29b2e35104b46
    Signed-off-by: Sravani Mada <quic_smada@quicinc.com>

--- msmnile_au.mk
+++ msmnile_au.mk
@@ -18,7 +18,7 @@ ifeq ($(ENABLE_VIRTUAL_AB), true)
 # Enable virtual A/B compression
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
-PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 endif
 
